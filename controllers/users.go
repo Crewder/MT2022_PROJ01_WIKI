b692d31c35ec53dc1c24a24a14ad6f4c332c1fe2
Florian <fleroy@focus-datascience.com>
2021-02-10 16:40:49 +0100

    Debut Creation AUTH JWT

--- controllers/users.go
+++ controllers/users.go
@@ -1,1 +1,86 @@
 package controllers
+
+import (
+	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gowiki-api/models"
+	"net/http"
+	"time"
+)
+
+// Fetch all User
+func UsersIndex(write http.ResponseWriter, request *http.Request) {
+
+	write.Header().Set("Content-type", "application/json;charset=UTF-8")
+	write.WriteHeader(http.StatusOK)
+	//TODO function allUser()
+	json.NewEncoder(write).Encode(Users.AllUser())
+}
+
+// Create a User
+func CreateUsers(write http.ResponseWriter, request *http.Request) {
+
+	write.Header().Set("Content-type", "application/json;charset=UTF-8")
+	write.WriteHeader(http.StatusOK)
+}
+
+//Create JWT Key
+var jwtKey = []byte("Ceci est un lapin et non un secret")
+
+// Create a struct for the request body
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+// Seed
+var Users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+// Auth the user
+// Return Cookie with JWT string
+func AuthUsers(write http.ResponseWriter, request *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(request.Body).Decode(&creds)
+
+	// Verify the structure of the body
+	if err != nil {
+		write.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Verify Password
+	expectedPassword, ok := Users[creds.Username]
+	if !ok || expectedPassword != creds.Password {
+		write.WriteHeader(http.StatusUnauthorized)
+	}
+
+	// Expiration Time Token
+	expirationTime := time.Now().Add(5 * time.Minute)
+
+	//Create the JWT Claims
+	claims := &jwt.Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+		Valid(StandardClaims),
+	}
+
+	// declare the JWT Token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		write.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// define the cookie
+	http.SetCookie(write, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+}
