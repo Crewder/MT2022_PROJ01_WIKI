68f3d55af67ff44f9a9b3730101353bc287fa007
Rémi Coufourier <remi.coufourier@hetic.net>
2021-02-10 16:17:33 +0100

    ajout des get

--- router.go
+++ router.go
@@ -14,6 +14,8 @@ func InitRouter() *mux.Router {
 	router.Methods("POST").Path("/comment").Name("CreateComment").HandlerFunc(controllers.CommentCreate)
 	router.Methods("POST").Path("/user").Name("CreateUser").HandlerFunc(controllers.UserCreate)
 	router.Methods("POST").Path("/auth").Name("Auth").HandlerFunc(controllers.UserAuth)
+	router.Methods("GET").Path("/article/{id}/view").Name("View").HandlerFunc(controllers.ArticleView)
+	router.Methods("GET").Path("/user/{id}").Name("ViewUser").HandlerFunc(controllers.ArticleView)
 
 	return router
 
