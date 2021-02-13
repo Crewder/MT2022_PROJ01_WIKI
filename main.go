3d9bd1f2d026cfc4e0a7d3b175015fff0fc8d16b
steven <steven.nativel@orange;fr>
2021-02-13 23:05:07 +0100

    Get all users and user by id

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gorilla/mux"
-	"github.com/gowiki-api/config"
 	"github.com/gowiki-api/controllers"
+	"net/http"
 )
 
 func main() {
-	config.DatabaseInit()
 	router := InitRouter()
 	http.ListenAndServe(":8080", router)
 }
@@ -25,8 +22,9 @@ func InitRouter() *mux.Router {
 	router.Methods("POST").Path("/comment").Name("CreateComment").HandlerFunc(controllers.CommentCreate)
 	router.Methods("POST").Path("/user").Name("CreateUser").HandlerFunc(controllers.UserCreate)
 	router.Methods("POST").Path("/auth").Name("Auth").HandlerFunc(controllers.UserAuth)
-	router.Methods("GET").Path("/article/{id}/view").Name("View").HandlerFunc(controllers.ArticleView)
-	router.Methods("GET").Path("/user/{id}").Name("ViewUser").HandlerFunc(controllers.UserView)*/
+	router.Methods("GET").Path("/article/{id}/view").Name("View").HandlerFunc(controllers.ArticleView)*/
+	router.Methods("GET").Path("/users").Name("ViewUser").HandlerFunc(controllers.GetUsers)
+	router.Methods("GET").Path("/user/{id}").Name("ViewUser").HandlerFunc(controllers.GetUser)
 
 	return router
 
--- models/user.go
+++ models/user.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"github.com/gowiki-api/config"
 	"gorm.io/gorm"
 )
+
+var db *gorm.DB
 
 type User struct {
 	gorm.Model
@@ -12,4 +15,20 @@ type User struct {
 	Password string
 }
 
-type Users []User
+func init() {
+	config.DatabaseInit()
+	db = config.GetDB()
+	db.AutoMigrate(&User{})
+}
+
+func GetAllUsers() []User {
+	var Users []User
+	db.Find(&Users)
+	return Users
+}
+
+func GetUserById(Id int64) *User {
+	var getUser User
+	db.Where("ID = ?", Id).Find(&getUser)
+	return &getUser
+}
