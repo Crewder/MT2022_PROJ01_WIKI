231bea7e78ddf01d9a4199e2c76190c26b942b0b
Camille Arsac <camille.arsac26@gmail.com>
2021-02-15 22:52:43 +0100

    show articles

--- controllers/articles.go
+++ controllers/articles.go
@@ -2,17 +2,32 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/gowiki-api/models"
+	"log"
 	"net/http"
+	"strconv"
 )
-
-func ArticleCreate(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("content-type", "application/json;json;charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "hello world", r.URL.Path[1:])
-
-}
 
 func Main(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello world", r.URL.Path[1:])
+}
+
+func GetArticles(w http.ResponseWriter, r *http.Request) {
+	articles := models.GetAllArticles()
+	coreResponse(w, http.StatusOK, articles)
+}
+
+func GetArticle(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	vars := mux.Vars(r)
+	articleId := vars["id"]
+	ID, err := strconv.ParseInt(articleId, 0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	articleDetails := models.GetArticleById(ID)
+
+	coreResponse(w, http.StatusOK, articleDetails)
 }
