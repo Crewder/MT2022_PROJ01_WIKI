c62c762be8a153d9624e01371b0c6337b5cbf6c7
Camille Arsac <camille.arsac26@gmail.com>
2021-02-15 22:52:43 +0100

    show articles

--- config/database.go
+++ config/database.go
@@ -1,13 +1,14 @@
 package config
 
 import (
-	"github.com/gowiki-api/models"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 	"os"
 )
+
+var db *gorm.DB
 
 func goDotEnvVariable(key string) string {
 	err := godotenv.Load(".env")
@@ -19,22 +20,23 @@ func goDotEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
-func DatabaseInit() {
+func init() {
 	var err error
 
-	var db *gorm.DB
-	var userDb = goDotEnvVariable("USER_DB")
-	var passwordDb = goDotEnvVariable("PASSWORD_DB")
-	var portDb = goDotEnvVariable("PORT_DB")
-	var nameDb = goDotEnvVariable("NAME_DB")
+	if db == nil {
+		var userDb = goDotEnvVariable("USER_DB")
+		var passwordDb = goDotEnvVariable("PASSWORD_DB")
+		var portDb = goDotEnvVariable("PORT_DB")
+		var nameDb = goDotEnvVariable("NAME_DB")
 
-	db, err = gorm.Open(mysql.Open(userDb+":"+passwordDb+"@tcp(127.0.0.1:"+portDb+")/"+nameDb+"?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+		db, err = gorm.Open(mysql.Open(userDb+":"+passwordDb+"@tcp(127.0.0.1:"+portDb+")/"+nameDb+"?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	db.AutoMigrate(&models.User{})
-	db.AutoMigrate(&models.Article{})
-	db.AutoMigrate(&models.Comment{})
+func GetDB() *gorm.DB {
+	return db
 }
--- models/article.go
+++ models/article.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/gowiki-api/config"
 	"gorm.io/gorm"
 )
 
@@ -13,3 +14,21 @@ type Article struct {
 }
 
 type Articles []Article
+
+var db = config.GetDB()
+
+func init() {
+	db.AutoMigrate(&Article{})
+}
+
+func GetAllArticles() []Article {
+	var Articles []Article
+	db.Find(&Articles)
+	return Articles
+}
+
+func GetArticleById(Id int64) *Article {
+	var article Article
+	db.Where("ID = ?", Id).Find(&article)
+	return &article
+}
