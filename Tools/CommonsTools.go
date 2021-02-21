682e86eb0c63bd7717f643974b696217fda07d03
Florian <fleroy@focus-datascience.com>
2021-02-21 18:20:40 +0100

    Create user + Hash PWD

---
+++ Tools/CommonsTools.go
@@ -0,0 +1,17 @@
+package Tools
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+func ComparePasswords(hashedPwd string, plainPwd []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
+}
